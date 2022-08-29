daaa3a6ba4ddf824f307281e6f523fb96efe5226
MatiasCermak <matias.cermak99@gmail.com>
2022-08-29 17:53:18 -0300

    Added upgrades to run file and go.mod file

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 solc --overwrite --abi contracts/implementation/Vote.sol -o ./build/abi
 solc --overwrite --bin contracts/implementation/Vote.sol -o ./build/bin
 
-abigen --bin=build/bin/Vote.bin --abi=build/abi/Vote.abi --pkg=api --out=./contracts/api/Vote.go
+abigen --bin=build/bin/Vote.bin --abi=build/abi/Vote.abi --pkg=api --out=./contracts/api/Vote.go && echo "Abigen run successful, created Vote.go in directory \"./contracts/api/Vote.go\""
